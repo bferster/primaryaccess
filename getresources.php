e23dbf07ced1ffeab1d0c1816dd199366f4c3f1a
bferster <bferster@stagetools.com>
2018-02-25 17:24:38 -0500

    pa db access

---
+++ getresources.php
@@ -0,0 +1,53 @@
+<?php
+header('Cache-Control: no-cache, no-store, must-revalidate'); 
+header('Expires: Sun, 01 Jul 2005 00:00:00 GMT'); 
+header('Pragma: no-cache'); 
+require_once('config.php');
+	
+	$q=addEscapes($_GET['q']);												
+	$era=addEscapes($_GET['era']);												
+	$query="SELECT * FROM resource WHERE ";							// Make query
+	if ($q) {														// If q spec'd
+		$query.="(title LIKE '%$q%' ";								
+		$query.="OR who LIKE '%$q%' OR what LIKE '%$q%')";
+		}
+	if ($era && $q)													// If both
+		$query.=" AND ";											// Add AND
+	if ($era)														// If era spec'd
+		$query.="era = '$era'";										// Match it
+	$result=mysql_query($query);									// Run query
+	if (($result == false) || (!mysql_numrows($result)))			// Error
+		print("Error");
+	else{															// Good result
+		$num=mysql_numrows($result);								// Get num rows
+		$str="GetPaRes([";											// Function
+		for ($i=0;$i<$num;$i++) {									// For each row
+			$str.="{id:\"".addEscapes(mysql_result($result,$i,"id"))."\",";			// Id
+			$str.="title:\"".addEscapes(mysql_result($result,$i,"title"))."\",";	// Title
+			$str.="desc:\"".addEscapes(mysql_result($result,$i,"who"))." ";			// Who
+			$str.=addslashes(mysql_result($result,$i,"what"))."\",";				// What
+			$str.="src:\"".addEscapes(mysql_result($result,$i,"src"))."\",";		// Src
+			$str.="link:\"".addEscapes(mysql_result($result,$i,"link"))."\",";		// Link
+			$str.="std:\"".addEscapes(mysql_result($result,$i,"standard"))."\","; 	// Std
+			$str.="era:\"".addEscapes(mysql_result($result,$i,"era"))."\"";// Era
+			$str.="}";
+			if ($i != $num-1)										// Not last one
+				$str.=",";											// Add comma
+			}
+		print($str."])");											// Print JSONP	
+		mysql_close();												// Close
+		}
+
+	function addEscapes($str)									// ESCAPE ENTRIES
+	{
+		if (!$str)												// If nothing
+			return $str;										// Quit
+		$str=addslashes($str);									// Add slashes
+		$str=str_replace("\r","",$str);							// No crs
+		$str=str_replace("\n","",$str);							// No lfs
+		return $str;
+	}
+
+/*	Rupert, Idaho. Former CCC (Civilian Conservation Corps) camp now under FSA (Farm Security Administration) management. Japanese-Americans taking down their flag in the evening
+*/
+?>
--- images.js
+++ images.js
@@ -65,88 +65,31 @@ CImageFind.prototype.ImportDialog=function(maxDocs, callback, mode)				// IMPORT
 			});
 	$("#mdEra").on("change", function() {											// ON CHANGE ERA
 			_this.era=$(this).val();												// Save era										
-			});
+			LoadCollection();														// Load it
+		});
 			
  	function LoadCollection() {													// LOAD COLLECTION FROM DB
-		var str;
-		_this.LoadingIcon(true,64);													// Show loading icon
-		var type=_this.type;														// Get type to show
-		if (type == "Images") type="Picture";										// Images are picture
-		var search="asset_type%3A%22"+type.toLowerCase()+"%22";						// Add asset type						
-		if (_this.filter) {															// If a filter spec'd
-			str="%22*"+_this.filter.toLowerCase()+"*%22";							// Search term
-			search+=" AND (title%3A"+str;											// Look at title
-			search+=" OR summary%3A"+str+")";										// Or summary
-			}
-		if (_this.filterCollect) {													// If a collection filter spec'd
-			str="%22*"+_this.filterCollect.toLowerCase()+"*%22";					// Search term
-			search+=" AND collection_title%3A"+str;									// Or collection title
-			}
-		if (_this.user) 															// If a user spec'd
-			search+=" AND node_user%3A*"+_this.user+"*";							// Look at user
-		var url="https://ss395824-us-east-1-aws.measuredsearch.com/solr/kmassets/select?"+"q="+search + 
-   				 "&fl=*&wt=json&json.wrf=?&rows="+_this.maxDocs+"&limit="+_this.maxDocs;
+		var era=5
+		var url="//viseyes.org/pa/getresources.php";
+		if (CImageFindObj.filter && CImageFindObj.era) url="?q="+CImageFindObj.filter+"&era="+era;				// Q and era
+		else if (CImageFindObj.filter) url+="?q="+CImageFindObj.filter;				// Q
+		else if (CImageFindObj.era) url+="?era="+era;								// Era
 
-		$.ajax( { url: url,  dataType: 'jsonp', jsonp: 'json.wrf' }).done(function(data) {
-			   		_this.FormatItems(data);
-			   		});
+		$.ajax( { url: url,  dataType: 'jsonp' });
 		}
-}																					// End closure
+	}																					// End closure
 
-CImageFind.prototype.FormatItems=function(data, sortBy)							// SHOW ITEMS
-{
-	var i,r,o;
-	var _this=this;																	// Save context
-	this.LoadingIcon(false);														// Hide loading icon
-	this.rawData=data;																// Save raw data
-	if (data) {																		// If not just sorting
-		this.data=[];																// New results store 
-		for (i=0;i<data.response.docs.length;++i) {									// For each doc returned
-			r=data.response.docs[i];												// Point at it
-			o={ title:"No title", desc:""};											// Create obj												
-			o.date=r.timestamp.substr(5,2)+"/"+r.timestamp.substr(8,2)+"/"+r.timestamp.substr(0,4);	// Munge date
-			if (r.title)															// If a title
-				o.title=r.title[0];													// Extract it
-			else if (r.caption)														// If a caption defined but no title
-				o.title=r.caption;													// Use it
-			o.id=r.id;																// Save id
-			o.thumb=r.url_thumb;													// Save thumb
-			if (o.thumb)	o.thumb=o.thumb.replace(/dev\-/,"");					// Remove 'dev-' prefix, if there
-			o.ajax=r.url_ajax;														// Save ajax
-			if (o.ajax)		o.ajax=o.ajax.replace(/dev\-/,"");						// Remove dev
-			o.json=r.url_json;														// Save json
-			if (o.json)		o.json=o.json.replace(/dev\-/,"");						// Remove dev
-			o.html=r.url_html;														// Save html
-			if (o.html)		o.html=o.html.replace(/dev\-/,"");						// Remove dev
-			o.embed=r.url_embed;													// Add embed 
-			if (o.embed) 	o.embed=o.embed.replace(/dev\-/,"");					// Remove dev
-			if ((r.asset_type == "picture") || (r.asset_type == "image")) {			// An image
-				if (r.url_huge)			o.ajax=r.url_huge.replace(/dev\-/,"");		// If huge, set url and remove 'dev-' prefix
-				else if (r.url_large)	o.ajax=r.url_large.replace(/dev\-/,"");		// Else use large
-				else if (r.url_normal)	o.ajax=r.url_normal.replace(/dev\-/,"");	// Else use normal
-				else 					o.ajax=o.thumb;								// Else use thumb
-				}
-			o.kmap=r.kmapid;														// Save kmap array
-			o.user=r.node_user;														// Add user
-			o.summary=r.summary;													// Add summary
-			o.type=r.asset_type;													// Add type
-			this.data.push(o);														// Add result to array
-			}
-		}
-	else if (this.data) {															// Just sorting and some data
-		var order=1;																// Assume ascending
-		if (this.data[0][sortBy] < this.data[this.data.length-1][sortBy])			// Ascending already
-			order=-1;																// Make it descending
-		this.data.sort(function(a,b) { 												// Reshuffle chairs on Titanic
-				if (a[sortBy] < b[sortBy])    	return -1*order;
-				else if (a[sortBy] > b[sortBy])	return 1*order;
-				else 			   				return 0;
-				});					
-		}
-	$("#numItemsFound").text(this.data.length);										// Show number of results
-	this.DrawAsGrid();																// Draw
-}
-
+	function GetPaRes(data)															// HADLE JSONP AJAX LOAD
+	{
+		var i,o;
+		CImageFindObj.LoadingIcon(false);											// Hide loading icon
+		CImageFindObj.data=[];														// New results store 
+		for (i=0;i<data.length;++i) 												// For each doc returned
+			CImageFindObj.data.push(data[i]);										// Add result to array
+		$("#numItemsFound").text(CImageFindObj.data.length);						// Show number of results
+		CImageFindObj.DrawAsGrid();													// Draw
+	}
+	
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // SHOW RESULTS
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -159,8 +102,8 @@ CImageFind.prototype.DrawAsGrid=function()											// SHOW RESULTS AS GRID
 		o=this.data[i];																// Point at doc
 		str+="<div class='pa-gridItem' id='mdres-"+i+"'>";							// Div start
 		str+="<div class='pa-gridPic'>";											// Pic div start
-		if (o.thumb)																// If a thumbnail defined
-			str+="<img src='"+o.thumb+"' width='100%'>";							// Add it
+		if (o.src)																	// If a thumbnail defined
+			str+="<img src='"+o.src+"' width='100%'>";								// Add it
 		str+="</div><span style='color:#27ae60'>"+(i+1)+". </span>";				// Add pic num
 		str+=this.ShortenString(o.title,70);										// Add title
 		str+="</div>";																// Close div	
@@ -176,9 +119,9 @@ CImageFind.prototype.DrawAsGrid=function()											// SHOW RESULTS AS GRID
 CImageFind.prototype.Preview=function(num)												// PREVIEW RESULT
 {
 	var o=this.data[num];																// Point at item
-	if (o.ajax)																			// If a pic
-		$("#addImg").prop("src",o.ajax);												// Show it
-	$("#addUrl").val(o.ajax);															// Src
+	if (o.src)																			// If a pic
+		$("#addImg").prop("src",o.src);													// Show it
+	$("#addUrl").val(o.src);															// Src
 	$("#addTitle").val(o.title);														// Title
 	$("#addDesc").val(o.desc);															// Desc
 	$("#addLink").val(o.html);															// Link
